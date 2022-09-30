c1fa4cb687bb585c226c02d4768ab3ba7806a506
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-01 01:27:45 +0600

    install react, firebase and paste here firebase config

---
+++ src/_steps.js
@@ -0,0 +1,8 @@
+/* 
+#Steps to use firebase
+1. create a project on console.firebase.google.com
+2. install firebase
+3. register web app in firebase
+4. 
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBvR3IOhQZEDoMmrsZwhYyQp2c-zRb03Nc",
+    authDomain: "simple-firebase-authenti-60bb4.firebaseapp.com",
+    projectId: "simple-firebase-authenti-60bb4",
+    storageBucket: "simple-firebase-authenti-60bb4.appspot.com",
+    messagingSenderId: "1063858960604",
+    appId: "1:1063858960604:web:b7a5ef51465625624c9c19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
