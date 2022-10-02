c77de988067f190dd0fa75c629940fb9990e1935
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-02 16:24:40 +0600

    add sign out funcationality and toggle sign in, sign out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
 import app from './firebase.init';
 import { useState } from 'react';
 
 const auth = getAuth(app);
 
+
 function App() {
+
   const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
@@ -20,11 +22,27 @@ function App() {
         console.error(error);
       })
   }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
 
   return (
+
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
-      <button>Sign Out</button>
+      {/* mane email thakle sign out button dekhabe, r email na thakle sign in button dekhabe */}
+      {
+        user.email ? <button onClick={handleSignOut}>Sign Out</button> :
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+
       <h2>User name: {user.displayName}</h2>
       <p>Email: {user.email}</p>
       <img src={user.photoURL} alt="user img" />
