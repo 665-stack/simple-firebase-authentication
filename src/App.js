214297125f5087d4983d3d0891f9d2a8d755ac44
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-02 15:39:01 +0600

    display user data after log in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
-
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
-        console.log(user)
+        setUser(user)
+        console.log(user);
       })
       .catch(error => {
-        console.log(error);
+        console.error(error);
       })
   }
 
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button>Sign Out</button>
+      <h2>User name: {user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="user img" />
     </div>
   );
 }
